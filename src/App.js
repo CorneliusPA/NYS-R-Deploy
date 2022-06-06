598149b74fae51ed49528a0e693155c517f0c0fa
Cornelius Allison <75234559+CorneliusPA@users.noreply.github.com>
2022-06-06 16:52:05 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,138 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import "./App.css";
+import { useState, useEffect } from "react";
+import Axios from "axios";
 
 function App() {
+const [name, setName] = useState("");
+  const [listOfProducts, setListOfProducts] = useState([]);
+  const [desc, setDesc] = useState("");
+  const [image, setImage] = useState("");
+  const [price, setPrice] = useState(0);
+  const [qty, setQty] = useState(0);
+  
+
+  useEffect(() => {
+    Axios.get("https://nys-react.herokuapp.com/getProducts").then((response) => {
+      setListOfProducts(response.data);
+    });
+  }, []);
+
+  const createProduct = () => {
+    Axios.post("https://nys-react.herokuapp.com/createProduct", {
+      name,
+      desc,
+      image,
+      price,
+      qty
+    }).then((response) => {
+      setListOfProducts([
+        ...listOfProducts,
+        {
+          name,
+          desc,
+          image,
+          price,
+          qty
+
+        }
+      ]);
+    });
+  };
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="App">
+
+<h1 style={{fontFamily:"Georgia", textAlign:"center"}}>New York Sity
+     <img style={{marginLeft:"10px"}} src="NYSLogo.png" alt="New York Sity" width="30px" height="30px"/>
+     </h1>
+
+
+
+    <Navbar/>
+    <div className="inputContainer2">
+<div className="inputContainer">
+      <div className="inputTable">
+
+         <label>Name:</label>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        />
+        <label>Desc:</label>
+         <input
+          type="text"
+          placeholder="Desc..."
+          onChange={(event) => {
+            setDesc(event.target.value);
+          }}
+        />
+<label>Image:</label>
+          <input
+          type="text"
+          placeholder="Image..."
+          onChange={(event) => {
+            setImage(event.target.value);
+          }}
+        />
+<label>Price:</label>
+        <input
+          type="number"
+          placeholder="Price..."
+          onChange={(event) => {
+            setPrice(event.target.value);
+          }}
+
+        />
+<label>Qty:</label>
+        <input
+          type="number"
+          placeholder="Quantity..."
+          onChange={(event) => {
+            setQty(event.target.value);
+          }}
+
+        />
+</div>
+        <button onClick={createProduct}> Create Product </button>
+
+      </div></div>
+<div className="productsDisplay2">
+      <div className="productsDisplay">
+        {listOfProducts.map((product) => {
+          return (
+            <div>
+               
+              <div className="feedContainer">
+                <div className="feedTable">
+             <img className="productImage" src= {product.image} alt="Product" ></img> 
+             <div className="productText">
+             <h3>Name: {product.name}</h3>
+              <h5>Desc: {product.desc}</h5> 
+              <h5>Price: ${product.price}</h5>
+              <h5>Qty: {product.qty}</h5>
+              </div>
+              </div>
+             </div>
+
+            </div>
+          );
+        })}
+      </div>
+</div>
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
