8384b249ce2e3b268c1578a0dda00b43e1ef4b2d
Cornelius Allison <75234559+CorneliusPA@users.noreply.github.com>
2022-06-10 12:03:46 -0700

    Applying Filtering Nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Navbar from "./Navbar";
 import "./App.css";
+import {BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import Axios from "axios";
 
@@ -10,13 +11,46 @@ const [name, setName] = useState("");
   const [image, setImage] = useState("");
   const [price, setPrice] = useState(0);
   const [qty, setQty] = useState(0);
+  const [dept, setDept] = useState(0)
   
-
+  
   useEffect(() => {
     Axios.get("https://nys-react.herokuapp.com/getProducts").then((response) => {
       setListOfProducts(response.data);
     });
   }, []);
+
+  const getProducts = () => {
+    Axios.get("https://nys-react.herokuapp.com/getProducts").then((response) => {
+      setListOfProducts(response.data);
+    });
+  };
+
+  const getChairs = () => {
+    Axios.get("https://nys-react.herokuapp.com/getChairs").then((response) => {
+      setListOfProducts(response.data);
+    });
+  };
+
+  const getCouches = () => {
+    Axios.get("https://nys-react.herokuapp.com/getCouches").then((response) => {
+      setListOfProducts(response.data);
+    });
+  };
+
+  const getSofas = () => {
+    Axios.get("https://nys-react.herokuapp.com/getSofas").then((response) => {
+      setListOfProducts(response.data);
+    });
+  };
+
+  const getOtherChairs = () => {
+    Axios.get("https://nys-react.herokuapp.com/getOtherChairs").then((response) => {
+      setListOfProducts(response.data);
+    });
+  };
+
+
 
   const createProduct = () => {
     Axios.post("https://nys-react.herokuapp.com/createProduct", {
@@ -24,7 +58,8 @@ const [name, setName] = useState("");
       desc,
       image,
       price,
-      qty
+      qty,
+      dept
     }).then((response) => {
       setListOfProducts([
         ...listOfProducts,
@@ -33,7 +68,8 @@ const [name, setName] = useState("");
           desc,
           image,
           price,
-          qty
+          qty,
+          dept
 
         }
       ]);
@@ -49,9 +85,21 @@ const [name, setName] = useState("");
      <img style={{marginLeft:"10px"}} src="NYSLogo.png" alt="New York Sity" width="30px" height="30px"/>
      </h1>
 
+  <table className='table'>
+    <tbody>
+            <tr>
+        <th onClick={getProducts}>Home</th>
+        <th onClick={getChairs}>Chairs</th>
+        <th onClick={getCouches}>Couches</th>
+        <th onClick={getSofas}>Sofas</th>
+        <th onClick={getOtherChairs}>More Chairs</th>
+        <th>Contacts</th>
+        </tr>
+   </tbody>
 
+        </table>
 
-    <Navbar/>
+   
     <div className="inputContainer2">
 <div className="inputContainer">
       <div className="inputTable">
@@ -80,6 +128,9 @@ const [name, setName] = useState("");
             setImage(event.target.value);
           }}
         />
+
+
+
 <label>Price:</label>
         <input
           type="number"
@@ -95,10 +146,21 @@ const [name, setName] = useState("");
           placeholder="Quantity..."
           onChange={(event) => {
             setQty(event.target.value);
-          }}
+          }}   
 
         />
+
+        <label>Dept:</label>
+        <input
+          type="number"
+          placeholder="Department..."
+          onChange={(event) => {
+            setDept(event.target.value);
+          }}/>
 </div>
+
+
+
         <button onClick={createProduct}> Create Product </button>
 
       </div></div>
@@ -116,6 +178,8 @@ const [name, setName] = useState("");
               <h5>Desc: {product.desc}</h5> 
               <h5>Price: ${product.price}</h5>
               <h5>Qty: {product.qty}</h5>
+              <h5>Dept: {product.dept}</h5>
+             
               </div>
               </div>
              </div>
